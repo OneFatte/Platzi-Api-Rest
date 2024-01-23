85c02b567fb50343c4480839278ba4501478dde3
OneFatte <camiloandres798@gmail.com>
2024-01-23 14:16:23 -0500

    Se crea version 2

--- app/Http/Controllers/Api/v1/PostController.php
+++ app/Http/Controllers/Api/v1/PostController.php
@@ -2,49 +2,29 @@
 
 namespace App\Http\Controllers\Api\v1;
 
+use App\Http\Resources\v1\PostResource;
 use App\Http\Controllers\Controller;
 use App\Models\Post;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
     public function index()
     {
-        //
+        return PostResource::collection(Post::latest()->paginate());
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     */
     public function show(Post $post)
     {
-        return $post;
+        return new PostResource($post);
     }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, Post $post)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
     public function destroy(Post $post)
     {
-        //
+        $post->delete();
+
+        // Respuesta con cosigos http
+        return response()->json([
+            'message' => 'Post deleted',
+        ], 204);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -4,4 +4,5 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\v1\PostController;
 
-Route::apiResource('v1/posts', PostController::class)->only('show');
+Route::apiResource('v1/posts', PostController::class)
+    ->only(['index', 'show', 'destroy']);
